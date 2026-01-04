ca9f927204076ad490b265dbbaa2a7a7aeeee905
Shawn <spapineau@spaptechnology.com>
2026-01-04 03:28:55 -0500

    landing page is nearly done

---
+++ client/src/data/placeholder.ts
@@ -0,0 +1,55 @@
+export const placeholderMerch = [
+	{
+		id: 1,
+		name: "License Plate Oversized Tee",
+		price: 45.0,
+		image: "images/merch_oversized.png",
+		description: "A comfortable tee featuring the iconic license plate design.",
+		category: "Apparel",
+		availableSizes: ["S", "M", "L", "XL"],
+		colors: ["black", "white"],
+	},
+	{
+		id: 2,
+		name: "Logomark Tee",
+		price: 45.0,
+		image: "images/merch_logomark.jpeg",
+		description: "Classic logomark tee for everyday wear.",
+		category: "Apparel",
+		availableSizes: ["S", "M", "L", "XL"],
+		colors: ["black", "white", "gray"],
+	},
+	{
+		id: 3,
+		name: "Dickies Logomark Work Shirt",
+		price: 70.0,
+		image: "images/merch_workshirt.png",
+		description: "Durable work shirt featuring the band logomark.",
+		category: "Apparel",
+		availableSizes: ["M", "L", "XL", "XXL"],
+		colors: ["navy", "gray"],
+	},
+];
+
+export const placeholderBands = [
+	{
+		id: 1,
+		image: "images/bands/adtr.jpg",
+	},
+	{
+		id: 2,
+		image: "images/bands/hardy.png",
+	},
+	{
+		id: 3,
+		image: "images/bands/lakeview.webp",
+	},
+	{
+		id: 4,
+		image: "images/bands/nickelback.png",
+	},
+	{
+		id: 5,
+		image: "images/bands/maylene.png",
+	},
+];
