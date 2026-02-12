cd4bdb3c23135a4f4c224d620c59144c90fe0d6a
Shawn <spapineau@spaptechnology.com>
2026-02-12 10:44:24 -0500

    changed config

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify"; // or '@astrojs/netlify-edge'
 
 import react from "@astrojs/react";
 
@@ -9,7 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
-	adapter: netlify(),
+	output: "static",
 
 	vite: {
 		plugins: [tailwindcss()],
